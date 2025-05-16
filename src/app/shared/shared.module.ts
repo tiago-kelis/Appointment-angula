93fd17920c466054d7b6322d248304a34b90bfcc
tiago-kelis <tiagokelis2017@gmail.com>
2025-05-16 17:32:01 -0300

    criando navbar com ngBootstrap

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
@@ -9,7 +10,8 @@ import { HeaderComponent } from './components/header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   exports: [
     HeaderComponent,
